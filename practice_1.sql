c8af10b395b7f19582b55a8e21e94354e055c909
LanhInTech <155951527+LanhInTech@users.noreply.github.com>
2024-01-08 16:52:42 +0700

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -39,8 +39,22 @@ order by employee_id asc
 select product_id from Products
 where low_fats = 'Y' and recyclable = 'Y'
 --ex10:
+select name from Customer
+where referee_id <> 2 OR referee_id IS NULL
 --ex11:
+SELECT NAME, POPULATION, AREA
+FROM WORLD
+WHERE AREA = 3000000 OR  population >= 25000000
 --ex12:
+SELECT DISTINCT AUTHOR_ID AS id FROM Views
+WHERE viewer_id >=1 AND VIEWER_ID = AUTHOR_ID
+ORDER BY AUTHOR_ID ASC
 --ex13:
+SELECT part, assembly_step FROM parts_assembly
+where finish_date is NULL;
 --ex14:
+SELECT * FROM lyft_drivers
+WHERE yearly_salary <= 30000 OR yearly_salary >= 70000;
 --ex15:
+select advertising_channel from uber_advertising
+WHERE money_spent > 100000 AND year = 2019;
