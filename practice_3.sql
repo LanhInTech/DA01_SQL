5f57de27260c1708c5d16a3b26706376badbbb05
LanhInTech <155951527+LanhInTech@users.noreply.github.com>
2024-01-15 09:50:17 +0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -26,8 +26,42 @@ FROM reviews
 group by product_ID, EXTRACT(MONTH FROM SUBMIT_DATE)
 ORDER BY EXTRACT(MONTH FROM SUBMIT_DATE), PRODUCT_ID;
 --ex5:
+SELECT
+SENDER_ID,
+COUNT(MESSAGE_ID) AS MESSAGE_COUNT
+FROM messages
+WHERE EXTRACT(MONTH FROM SENT_DATE) = 8 
+AND EXTRACT(YEAR FROM SENT_DATE) = 2022
+GROUP BY SENDER_ID
+ORDER BY MESSAGE_COUNT DESC
+LIMIT 2;
 --ex6:
+SELECT 
+TWEET_ID
+FROM TWEETS
+WHERE LENGTH(CONTENT) > 15
 --ex7:
+SELECT 
+ACTIVITY_DATE AS day,
+COUNT(distinct user_id) as active_users
+FROM ACTIVITY
+WHERE activity_date BETWEEN '2019-06-27' AND '2019-07-27'
+AND SESSION_ID >= 1
+GROUP BY ACTIVITY_DATE;
 --ex8:
+select count(id), joining_date
+from employees
+where extract(month from joining_date) between '01' and '07'
+and extract (year from joining_date) = '2022'
+group by joining_date;
 --ex9:
+select 
+first_name,
+position('a' in first_name)
+from worker;
 --ex10:
+select
+title,
+substring(title from length(winery)+2 for 4) as vintage_year 
+from winemag_p2
+where country = 'Macedonia';
