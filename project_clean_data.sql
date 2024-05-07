b7cf954babb26decdcc52e01cafee18dec4f252d
LanhInTech <155951527+LanhInTech@users.noreply.github.com>
2024-05-07 23:02:05 +0700

    Update project_clean_data.sql

--- project_clean_data.sql
+++ project_clean_data.sql
@@ -10,6 +10,8 @@ ALTER COLUMN orderlinenumber TYPE numeric USING orderlinenumber::numeric;
 ALTER TABLE sales_dataset_rfm_prj
 ALTER COLUMN sales TYPE decimal USING sales::decimal;
 ALTER TABLE sales_dataset_rfm_prj
+ALTER COLUMN orderdate TYPE date USING orderdate::date;
+ALTER TABLE sales_dataset_rfm_prj
 ALTER COLUMN msrp TYPE numeric USING msrp::numeric;
 
 SELECT * FROM public.sales_dataset_rfm_prj;
@@ -43,9 +45,37 @@ UPDATE public.sales_dataset_rfm_prj SET contactfirstname = INITCAP(contactfirstn
 
 --4) Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
 ALTER TABLE public.sales_dataset_rfm_prj
-ADD COLUMN QTR_ID text; 
+ADD COLUMN QTR_ID numeric; 
 ALTER TABLE public.sales_dataset_rfm_prj
-ADD COLUMN MONTH_ID text;
+ADD COLUMN MONTH_ID numeric;
 ALTER TABLE public.sales_dataset_rfm_prj
-ADD COLUMN YEAR_ID text;
+ADD COLUMN YEAR_ID numeric;
+
+UPDATE public.sales_dataset_rfm_prj SET month_id = EXTRACT(month FROM orderdate);
+UPDATE public.sales_dataset_rfm_prj SET qtr_id = EXTRACT(QUARTER FROM orderdate);
+UPDATE public.sales_dataset_rfm_prj SET year_id = EXTRACT(year FROM orderdate);
+
+--5) Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách)
+WITH twt_count as(
+SELECT 
+*, (q1 - 1.5*IQR) min_value, (q3 + 1.5*IQR) max_value
+FROM
+(
+SELECT PERCENTILE_CONT(0.25)
+WITHIN GROUP (ORDER BY quantityordered) q1, 
+PERCENTILE_CONT(0.75)
+WITHIN GROUP (ORDER BY quantityordered) q3,
+(PERCENTILE_CONT(0.75)
+WITHIN GROUP (ORDER BY quantityordered) 
+- 
+PERCENTILE_CONT(0.25)
+WITHIN GROUP (ORDER BY quantityordered)) IQR
+FROM public.sales_dataset_rfm_prj) AS a_table)
+
+SELECT * FROM public.sales_dataset_rfm_prj
+WHERE quantityordered < (SELECT min_value FROM twt_count) 
+OR quantityordered > (SELECT max_value FROM twt_count)
+
+
+
 
