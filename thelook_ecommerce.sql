eae8ffe5dc27bb24191717ec4f2d07af2171b565
LanhInTech <155951527+LanhInTech@users.noreply.github.com>
2024-06-04 16:36:44 +0700

    Update and rename project_2.sql to thelook_ecommerce.sql

---
+++ thelook_ecommerce.sql
@@ -0,0 +1,4 @@
+-- 1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
+Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng (Từ 1/2019-4/2022)
+Output: month_year (yyyy-mm) , total_user, total_order ---
+
