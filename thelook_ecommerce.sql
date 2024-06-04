7da715e4de61ae41bda5745b2f090b13df06443c
LanhInTech <155951527+LanhInTech@users.noreply.github.com>
2024-06-04 17:01:00 +0700

    Update thelook_ecommerce_p1.sql

--- thelook_ecommerce.sql
+++ thelook_ecommerce.sql
@@ -1,4 +1,33 @@
--- 1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
+-- Thống kê tháng P1: Số lượng đơn hàng và số lượng khách hàng mỗi tháng
 Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng (Từ 1/2019-4/2022)
 Output: month_year (yyyy-mm) , total_user, total_order ---
+
+SELECT 
+
+-- Thống kê tháng P2: Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
+Thống kê giá trị đơn hàng trung bình và tổng số người dùng khác nhau mỗi tháng (Từ 1/2019-4/2022)
+Output: month_year ( yyyy-mm), distinct_users, average_order_value ---
+
+SELECT
+
+-- Thống kê khách hàng: Nhóm khách hàng theo độ tuổi
+Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính (Từ 1/2019-4/2022)
+Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
+
+SELECT
+
+-- Thống kê sản phẩm: Top 5 sản phẩm mỗi tháng.
+Thống kê top 5 sản phẩm có lợi nhuận cao nhất từng tháng (xếp hạng cho từng sản phẩm). 
+Output: month_year ( yyyy-mm), product_id, product_name, sales, cost, profit, rank_per_month
+
+SELECT
+
+-- Thống kê doanh thu: Doanh thu tính đến thời điểm hiện tại trên mỗi danh mục
+Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua (giả sử ngày hiện tại là 15/4/2022)
+Output: dates (yyyy-mm-dd), product_categories, revenue
+
+SELECT
+
+
+
 
